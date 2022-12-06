c2482eee98414d21dfa8433c5fdc3410444dd1de
jiingom <vvankatam0@gmail.com>
2022-12-06 21:36:06 +0900

    feat(Judgement): 맞춘 숫자의 개수를 구하는 기능 구현

--- src/main/kotlin/baseball/domain/Judgement.kt
+++ src/main/kotlin/baseball/domain/Judgement.kt
@@ -1,5 +1,13 @@
 package baseball.domain
 
 class Judgement {
-
+    fun correctCount(computer: List<Int>, player: List<Int>): Int {
+        var result = 0
+        for (i in computer.indices) {
+            if (computer.contains(player[i])) {
+                result++
+            }
+        }
+        return result
+    }
 }
