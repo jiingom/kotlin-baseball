125ae87f135cd66b15a9c015055e07b63e16fd41
jiingom <vvankatam0@gmail.com>
2022-12-06 22:10:58 +0900

    refactor(NumberGenerator): 반환 타입을 immutable로 변경

--- src/main/kotlin/baseball/domain/NumberGenerator.kt
+++ src/main/kotlin/baseball/domain/NumberGenerator.kt
@@ -3,7 +3,7 @@ package baseball.domain
 import camp.nextstep.edu.missionutils.Randoms
 
 class NumberGenerator {
-    fun createRandomNumbers(): MutableList<Int> {
+    fun createRandomNumbers(): List<Int> {
         val computerNum = mutableListOf<Int>()
         while (computerNum.size < 3) {
             val randomNumber = Randoms.pickNumberInRange(1, 9)
