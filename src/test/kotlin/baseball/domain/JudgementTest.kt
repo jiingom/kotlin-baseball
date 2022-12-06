be630f930003f34b1aec8b8e0cf23c30289b0a94
jiingom <vvankatam0@gmail.com>
2022-12-06 21:37:36 +0900

    test(Judgement): 특정 자리에 특정 숫자가 있는지 확인하는 테스트

--- src/test/kotlin/baseball/domain/JudgementTest.kt
+++ src/test/kotlin/baseball/domain/JudgementTest.kt
@@ -8,4 +8,9 @@ internal class JudgementTest {
     fun `맞춘 개수를 반환한다`() {
         assertThat(Judgement().correctCount(listOf(1,2,3), listOf(3,1,2))).isEqualTo(3)
     }
+
+    @Test
+    fun `특정 자리에 특정 숫자가 있는지 확인한다(스트라이크)`() {
+        assertThat(Judgement().hasPlace(listOf(1,2,3), 0, 1)).isTrue
+    }
 }
