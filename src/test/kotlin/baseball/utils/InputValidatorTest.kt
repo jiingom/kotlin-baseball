47b15da1deeaf84ae40eba17e515355e96f68011
jiingom <vvankatam0@gmail.com>
2022-12-06 18:43:08 +0900

    test(InputValidator): 플레이어의 수를 입력받는 기능 테스트

---
+++ src/test/kotlin/baseball/utils/InputValidatorTest.kt
@@ -0,0 +1,19 @@
+package baseball.utils
+
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+internal class InputValidatorTest {
+    @ValueSource(strings = ["abc", "12a"])
+    @ParameterizedTest
+    fun `1~9사이의 숫자가 아닌 경우 예외가 발생한다`(input: String) {
+        assertThrows<IllegalArgumentException> { InputValidator().isCorrectPlayerNumbers(input) }
+    }
+
+    @ValueSource(strings = ["1", "12", "1234"])
+    @ParameterizedTest
+    fun `3자리의 수가 아닌 경우 예외가 발생한다`(input: String) {
+        assertThrows<IllegalArgumentException> { InputValidator().isCorrectPlayerNumbers(input) }
+    }
+}
