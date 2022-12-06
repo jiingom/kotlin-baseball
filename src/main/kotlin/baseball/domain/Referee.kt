a0bfcacb9b3de96ff8dd146e5bc8287ee173d813
jiingom <vvankatam0@gmail.com>
2022-12-06 22:01:19 +0900

    feat(Referee): 게임을 클리어했는지 여부 확인 기능 구현

--- src/main/kotlin/baseball/domain/Referee.kt
+++ src/main/kotlin/baseball/domain/Referee.kt
@@ -1,5 +1,32 @@
 package baseball.domain
 
-class Referee {
+import baseball.view.OutputView
 
+class Referee {
+    private val judgement = Judgement()
+    fun compare(computer: List<Int>, player: List<Int>): Boolean {
+        val correctCount = judgement.correctCount(computer, player)
+        var strike = calculateStrike(computer, player)
+        var ball = correctCount - strike
+        OutputView().printResult(strike, ball)
+        return isGameClear(strike)
+    }
+
+    private fun calculateStrike(computer: List<Int>, player: List<Int>): Int {
+        var strike = 0
+        for (i in player.indices) {
+            if (judgement.hasPlace(computer, i, player[i])) {
+                strike++
+            }
+        }
+        return strike
+    }
+
+    private fun isGameClear(strike: Int): Boolean {
+        if (strike == 3) {
+            OutputView().printClearMessage()
+            return true
+        }
+        return false
+    }
 }
